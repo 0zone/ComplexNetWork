ca6d2c002aaa39dcde815bd17b9f9ac95ba45f50
jinyu <yupengjin@gmail.com>
2016-03-21 16:12:59 +0800

    kendall_tau

--- Egonet.py
+++ Egonet.py
@@ -6,6 +6,7 @@ import MySQLdb
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as stats
 
 current_path = "D:\\ComplexNetwork"
 node_enw_file_name = current_path + "\\result\\ego_pro\\"
@@ -97,6 +98,24 @@ def get_gsm_network(begin_date, time_scale):
     return network
 
 
+def get_sms_network(begin_date, time_scale):
+    table_name = 'aba_sms'
+    conn = MySQLdb.connect(host="localhost", user="root", passwd="root", db="network", charset="utf8")
+    sql_select = 'SELECT * FROM ' + table_name + ' WHERE date_index >= %s and date_index < %s'
+
+    select_cur = conn.cursor()
+    select_cur.execute(sql_select, (begin_date, begin_date + time_scale))
+    result_data = select_cur.fetchall()
+    # 构建网络
+    network = nx.Graph()
+    for row in result_data:
+        network.add_edge(row[1], row[2])
+    select_cur.close()
+    conn.commit()
+    conn.close()
+    return network
+
+
 def get_network_from_file(network_file_name):
     network_file = open(network_file_name, 'r')
 
@@ -160,10 +179,21 @@ def iter_filter(n_vec, e_vec, a, c):
     return it_ls_fit
 
 
+def get_score(score_file_name, node_num, n_vec, e_vec, c, a):
+    score_file = open(score_file_name, "w")
+    n_count = 0
+    for x in n_vec:
+        y = e_vec[n_count]
+        score = max((10**c) * (x**a), y) / min((10**c) * (x**a), y) * math.log(math.fabs((10**c) * (x**a)-y)+1)
+        score_file.write(node_num[n_count] + "," + str(score) + "\n")
+        n_count += 1
+    score_file.close()
+
+
 def plot_en(en_file_name, node_cnt):
     bin_count = 10
     en_file = open(en_file_name, 'r')
-    node_num_dict = {}
+    node_num = []
     num_score = np.zeros(node_cnt)
     n_v = np.zeros(node_cnt)
     e_v = np.zeros(node_cnt)
@@ -172,7 +202,7 @@ def plot_en(en_file_name, node_cnt):
     line_cnt = 0
     for line in en_file:
         data = line.split(' ')
-        node_num_dict[data[0]] = line_cnt
+        node_num.append(data[0].strip())
 
         n_v[line_cnt] = float(data[1])
         e_v[line_cnt] = float(data[2])
@@ -197,12 +227,14 @@ def plot_en(en_file_name, node_cnt):
     print med_n
     print med_e
     # 中位数拟合
-    ls_fit = np.polyfit(np.log10(med_n[0:6]), np.log10(med_e[0:6]), 1)
+    ls_fit = np.polyfit(np.log10(drop_zeros(med_n)), np.log10(drop_zeros(med_e)), 1)
     ls_x = range(1, 10000)
     ls_y = []
     for x in ls_x:
         ls_y.append((10**ls_fit[1])*(x**ls_fit[0]))
     print ls_fit
+
+    get_score(en_file_name + "-score.csv", node_num, n_v, e_v, ls_fit[1], ls_fit[0])
     # new_ls = ls_fit
     # for i in range(0, 10):
     #     new_ls = iter_filter(n_v, e_v, new_ls[0], new_ls[1])
@@ -227,28 +259,183 @@ def plot_en(en_file_name, node_cnt):
 
     # plt.loglog(n_v, e_v, 'c.', ms=5)
     #
-    plt.loglog(med_n, med_e, "k.", ms=10)
+    plt.plot(n_v, e_v, "c.")
     plt.loglog(ls_x, ls_y, 'r', linewidth=2)    # 均值拟合曲线
+    plt.loglog(med_n, med_e, "k.", ms=10)
     # plt.loglog(ls_x1, ls_y1, 'y', linewidth=2)    # 直接拟合曲线
     # plt.loglog(slop1_x, slop1_y, 'y', linewidth=2)
     # plt.loglog(slop2_x, slop2_y, 'b', linewidth=2)  #best 1.3
     # plt.loglog(range(1, 5000, 1), range(1, 5000, 1), 'b', linewidth=2)  #slop1
-    plt.plot(n_v, e_v, "c.")
-    plt.loglog([2, 2], [1, 100000],  'k--')    # dash line
+
+    # plt.loglog([2, 2], [1, 100000],  'k--')    # dash line
     plt.show()
 
-# def plot_en(en_file_name, node_cnt):
-#     en_file = open(en_file_name, 'r')
-#     n_v = []
-#     e_v = []
-#
-#     for line in en_file:
-#
-#         data = line.split(' ')
-#         n_v.append(float(data[1]))
-#         e_v.append(float(data[2]))
-#         if float(data[1]) > 100 and data[1] == data[2]:
-#             print data[0]
+
+#  归一化社团ego_pro
+# c_sum为非孤立社团数量
+def normal_c_pro(large_scale_node_c_file_name, large_scale_ego_pro_file_name, norm_ego_pro_file_name, c_sum):
+    # 社团节点计数
+    c_node_sum = np.zeros(c_sum)
+    large_scale_node_c_file = open(large_scale_node_c_file_name, 'r')
+    line_cnt = 0
+    for node_c_line in large_scale_node_c_file:
+        node_c_data = node_c_line.split(" ")
+        c_num = int(node_c_data[1].strip())
+        c_node_sum[c_num] += 1
+        line_cnt += 1
+    large_scale_node_c_file.close()
+
+    # egonet点数边数归一
+    large_scale_norm_ego_pro_file = open(norm_ego_pro_file_name, 'w')
+    large_scale_ego_pro_file = open(large_scale_ego_pro_file_name, 'r')
+    for c_pro_line in large_scale_ego_pro_file:
+        c_pro_data = c_pro_line.split(" ")
+        c_num = int(c_pro_data[0].strip())
+        c_n = float(c_pro_data[1].strip())
+        c_e = float(c_pro_data[2].strip())
+        small_scale_node_cnt = c_node_sum[c_num]            # 社团节点数量
+        norm_n = c_n / small_scale_node_cnt
+        norm_e = c_e / small_scale_node_cnt
+
+        large_scale_norm_ego_pro_file.write(str(c_num) + " " + str(norm_n) + " " + str(norm_e) + "\n")
+    large_scale_ego_pro_file.close()
+    large_scale_norm_ego_pro_file.close()
+
+
+# 大尺度社团异常个体数量和比例
+# c_sum为社团数量
+def statistics_c_small_anomaly_node_sum(small_scale_score_file_name,
+                                        c_small_anomaly_file_name,
+                                        large_scale_node_c_file_name,
+                                        large_scale_score_file_name,
+                                        c_sum,
+                                        sigma_thres):
+    # 小尺度节点分数
+    node_score = {}
+    small_scale_score_file = open(small_scale_score_file_name, 'r')
+    for node_score_line in small_scale_score_file:
+        node_score_data = node_score_line.split(",")
+        node_num = node_score_data[0].strip()
+        score = float(node_score_data[1].strip())
+        node_score[node_num] = score
+    small_scale_score_file.close()
+    # 分数方差、均值
+    temp = np.zeros(len(node_score))
+    index = 0
+    for score in node_score.values():
+        temp[index] = score
+        index += 1
+    avg = np.mean(temp)
+    std = np.std(temp)
+    thres = avg + sigma_thres * std
+
+    # 社团节点计数,异常点计数
+    c_node_sum = np.zeros(c_sum)
+    c_anomaly_node_sum = np.zeros(c_sum)
+    large_scale_node_c_file = open(large_scale_node_c_file_name, 'r')
+    line_cnt = 0
+    for node_c_line in large_scale_node_c_file:
+        node_c_data = node_c_line.split(" ")
+        node_num = node_c_data[0].strip()
+        c_num = int(node_c_data[1].strip())
+        c_node_sum[c_num] += 1
+        if node_score[node_num] > thres:
+            c_anomaly_node_sum[c_num] += 1
+        line_cnt += 1
+    large_scale_node_c_file.close()
+
+    large_scale_score_file = open(large_scale_score_file_name, 'r')
+    c_small_anomaly_file = open(c_small_anomaly_file_name, 'w')
+    for large_scale_score_line in large_scale_score_file:
+        large_scale_score_data = large_scale_score_line.split(",")
+        c_num = int(large_scale_score_data[0].strip())
+        c_score = float(large_scale_score_data[1].strip())
+        node_sum = c_node_sum[c_num]
+        anomaly_node_sum = c_anomaly_node_sum[c_num]
+        anomaly_ratio = anomaly_node_sum / node_sum
+        c_small_anomaly_file.write(str(c_num) + "," + str(c_score) + "," + str(node_sum) + "," + str(anomaly_node_sum) + "," + str(anomaly_ratio)+"\n")
+    c_small_anomaly_file.close()
+
+
+# 大尺度社团分数
+# c_sum为社团数量
+def statistics_c_small_sum_scroe(small_scale_score_file_name, c_small_sum_score_file_name, large_scale_node_c_file_name, c_sum):
+    node_score = {}
+    c_small_sum_score = np.zeros(c_sum)
+    small_scale_score_file = open(small_scale_score_file_name, 'r')
+    for node_score_line in small_scale_score_file:
+        node_score_data = node_score_line.split(",")
+        node_num = node_score_data[0].strip()
+        score = float(node_score_data[1].strip())
+        node_score[node_num] = score
+    small_scale_score_file.close()
+
+    large_scale_node_c_file = open(large_scale_node_c_file_name, 'r')
+    line_cnt = 0
+    for node_c_line in large_scale_node_c_file:
+        node_c_data = node_c_line.split(" ")
+        node_num = node_c_data[0].strip()
+        c_num = int(node_c_data[1].strip())
+        c_small_sum_score[c_num] += node_score[node_num]
+        line_cnt += 1
+        print line_cnt
+    large_scale_node_c_file.close()
+    print "sum"
+    c_small_sum_score_file = open(c_small_sum_score_file_name, 'w')
+    for c_index in range(0, c_sum):
+        c_small_sum_score_file.write(str(c_index) + "," + str(c_small_sum_score[c_index]) + "\n")
+    c_small_sum_score_file.close()
+
+
+# 团体异常相关性分析
+def anomal_scroe_correlation(c_small_anomaly_file_name, c_sum):
+    c_score = np.zeros(c_sum)
+    anaomaly_node_ratio = np.zeros(c_sum)
+
+    c_small_anomaly_file = open(c_small_anomaly_file_name, 'r')
+    for c_small_anomaly_line in c_small_anomaly_file:
+        c_small_anomaly_data = c_small_anomaly_line.split(",")
+        c_num = int(c_small_anomaly_data[0])
+        c_score[c_num] = float(c_small_anomaly_data[1])
+        anaomaly_node_ratio[c_num] = float(c_small_anomaly_data[4])
+    c_small_anomaly_file.close()
+
+    # correlation_analysis
+    drop_zeros(c_score)
+    drop_zeros(anaomaly_node_ratio)
+    p_c_cnt = 0.0
+    n_c_cnt = 0.0
+    pair_cnt = 0.0
+    for i in range(0, len(c_score)-1):
+        current_score = c_score[i]
+        current_ratio = anaomaly_node_ratio[i]
+        for j in range(i+1, len(c_score)):
+            score = c_score[j]
+            ratio = anaomaly_node_ratio[j]
+            if ((current_score > score) and (current_ratio > ratio)) or ((current_score < score) and (current_ratio < ratio)):
+                p_c_cnt += 1
+            if ((current_score > score) and (current_ratio < ratio)) or ((current_score < score) and (current_ratio > ratio)):
+                n_c_cnt += 1
+            pair_cnt += 1
+            if pair_cnt % 100000 == 0:
+                print pair_cnt
+    print p_c_cnt, n_c_cnt, pair_cnt
+    print (p_c_cnt-n_c_cnt)/pair_cnt
+
+
+def kendall_tau(score_ratio_file_name):
+    c_score = []
+    anaomaly_node_ratio = []
+
+    c_small_anomaly_file = open(score_ratio_file_name, 'r')
+    for c_small_anomaly_line in c_small_anomaly_file:
+        c_small_anomaly_data = c_small_anomaly_line.split(",")
+        c_num = int(c_small_anomaly_data[0])
+        c_score.append(float(c_small_anomaly_data[1]))
+        anaomaly_node_ratio.append(float(c_small_anomaly_data[4]))
+    c_small_anomaly_file.close()
+    tau, p_value = stats.kendalltau(c_score, anaomaly_node_ratio)
+    print tau, p_value
 
 begin_date = 305
 time_scale = 7
@@ -256,20 +443,86 @@ time_scale = 7
 # network = get_gsm_network(begin_date, time_scale)
 # ego_stat(network, ego_pro_file_name)
 # plot_en(node_enw_file_name + "305-7.txt", 332826)
+#
+# plot_en(node_enw_file_name + "305-7-level0.txt-ego-pro.txt", 24060)
+# plot_en(node_enw_file_name + "305-7-level1.txt-ego-pro.txt", 3849)
+# plot_en(node_enw_file_name + "305-7-level2.txt-ego-pro.txt", 739)
+# plot_en(node_enw_file_name + "305-7-level3.txt-ego-pro.txt", 523)
 
-network_file_name = node_enw_file_name + "305-7-level0.txt"
-network = get_network_from_file(network_file_name)
-ego_stat(network, network_file_name + "-ego-pro.txt")
+# statistics_c_small_sum_scroe(node_enw_file_name + "305-7.txt-score.csv",
+#                              node_enw_file_name + "305-7-level0-c-sum-score.csv",
+#                              node_enw_file_name + "305-7-level0-node-c.txt",
+#                              27998)
 
-network_file_name = node_enw_file_name + "305-7-level1.txt"
-network = get_network_from_file(network_file_name)
-ego_stat(network, network_file_name + "-ego-pro.txt")
+# plot_en(node_enw_file_name + "305-7-level0.txt-ego-pro.txt", 24060)
 
-network_file_name = node_enw_file_name + "305-7-level2.txt"
-network = get_network_from_file(network_file_name)
-ego_stat(network, network_file_name + "-ego-pro.txt")
 
-network_file_name = node_enw_file_name + "305-7-level3.txt"
-network = get_network_from_file(network_file_name)
-ego_stat(network, network_file_name + "-ego-pro.txt")
+# level-0
+# norm
+# normal_c_pro(node_enw_file_name + "305-7-level0-node-c.txt",
+#              node_enw_file_name + "305-7-level0.txt-ego-pro.txt",
+#              node_enw_file_name + "305-7-level0.txt-ego-pro.txt" + "-norm.txt",
+#              27998)
+# plot_en(node_enw_file_name + "305-7-level0.txt-ego-pro.txt" + "-norm.txt", 24060)
 
+# statistics_c_small_anomaly_node_sum(node_enw_file_name + "305-7.txt-score.csv",
+#                              node_enw_file_name + "305-7-level0-c-anomaly-ratio.csv",
+#                              node_enw_file_name + "305-7-level0-node-c.txt",
+#                              node_enw_file_name + "305-7-level0.txt-ego-pro.txt-norm.txt-score.csv",
+#                              27998,
+#                              3)
+# anomal_scroe_correlation(node_enw_file_name+"305-7-level0-c-anomaly-ratio.csv", 27998)
+
+
+# level-1
+# norm
+# normal_c_pro(node_enw_file_name + "305-7-level1-node-c.txt",
+#              node_enw_file_name + "305-7-level1.txt-ego-pro.txt",
+#              node_enw_file_name + "305-7-level1.txt-ego-pro.txt" + "-norm.txt",
+#              7934)
+# plot_en(node_enw_file_name + "305-7-level1.txt-ego-pro.txt" + "-norm.txt", 3849)
+# statistics_c_small_anomaly_node_sum(node_enw_file_name + "305-7.txt-score.csv",
+#                              node_enw_file_name + "305-7-level1-c-anomaly-ratio.csv",
+#                              node_enw_file_name + "305-7-level1-node-c.txt",
+#                              node_enw_file_name + "305-7-level1.txt-ego-pro.txt-norm.txt-score.csv",
+#                              7934,
+#                              3)
+# anomal_scroe_correlation(node_enw_file_name+"305-7-level1-c-anomaly-ratio.csv", 7934)
+
+
+# level-2
+# norm
+# normal_c_pro(node_enw_file_name + "305-7-level2-node-c.txt",
+#              node_enw_file_name + "305-7-level2.txt-ego-pro.txt",
+#              node_enw_file_name + "305-7-level2.txt-ego-pro.txt" + "-norm.txt",
+#              4828)
+# plot_en(node_enw_file_name + "305-7-level2.txt-ego-pro.txt" + "-norm.txt", 739)
+# statistics_c_small_anomaly_node_sum(node_enw_file_name + "305-7.txt-score.csv",
+#                              node_enw_file_name + "305-7-level2-c-anomaly-ratio.csv",
+#                              node_enw_file_name + "305-7-level2-node-c.txt",
+#                              node_enw_file_name + "305-7-level2.txt-ego-pro.txt-norm.txt-score.csv",
+#                              4828,
+#                              3)
+# anomal_scroe_correlation(node_enw_file_name+"305-7-level2-c-anomaly-ratio.csv", 4828)
+
+# level-3
+# norm
+# normal_c_pro(node_enw_file_name + "305-7-level3-node-c.txt",
+#              node_enw_file_name + "305-7-level3.txt-ego-pro.txt",
+#              node_enw_file_name + "305-7-level3.txt-ego-pro.txt" + "-norm.txt",
+#              4612)
+# plot_en(node_enw_file_name + "305-7-level3.txt-ego-pro.txt" + "-norm.txt", 523)
+# statistics_c_small_anomaly_node_sum(node_enw_file_name + "305-7.txt-score.csv",
+#                              node_enw_file_name + "305-7-level3-c-anomaly-ratio.csv",
+#                              node_enw_file_name + "305-7-level3-node-c.txt",
+#                              node_enw_file_name + "305-7-level3.txt-ego-pro.txt-norm.txt-score.csv",
+#                              4612,
+#                              3)
+# anomal_scroe_correlation(node_enw_file_name+"305-7-level3-c-anomaly-ratio.csv", 4612)
+kendall_tau(node_enw_file_name+"305-7-level3-c-anomaly-ratio.csv")
+
+# file_path = "E:/新建文件夹/305-7.txt"
+# file = open(file_path, 'w')
+# network = get_gsm_network(begin_date, time_scale)
+# for edge in network.edges():
+#     file.write(str(edge[0]) + " " + str(edge[1]) + "\n")
